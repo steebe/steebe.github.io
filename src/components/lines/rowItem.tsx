b3921bc6a251540770ce08a674a2a7e77396b947
Stephen Bass <steve@stevebass.me>
2025-11-21 01:13:31 -0500

    blah blah blah whatever

--- src/components/lines/rowItem.tsx
+++ src/components/lines/rowItem.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Link } from "gatsby";
 import { Node } from "../../types/node";
 import * as styles from "./rowItem.module.css";
